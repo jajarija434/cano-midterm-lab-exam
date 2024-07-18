5eadb141cc762a9deaa1548b88cbc7eb59750823
jajarija434 <jajacano71@gmail.com>
2024-07-18 12:06:00 +0800

    Initial Commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\comment_replies;
+use App\Http\Controllers\post_comments;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resources('comment_replies', comment_repliesController::class );
+Route::resources('post_comments', Post_commentsController::class );
