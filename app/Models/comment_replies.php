1833814e3f3c7f9d97955ff69469dffb95fe6d33
jajarija434 <jajacano71@gmail.com>
2024-07-18 11:25:10 +0800

    Initial Commit

---
+++ app/Models/comment_replies.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+use App\Models\comment_replies;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class comment_replies extends Model
+{
+    use HasFactory;
+}
+ 
---
+++ app/Models/post_comments.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+use App\Models\post_comments;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class post_comments extends Model
+{
+    use HasFactory;
+}
