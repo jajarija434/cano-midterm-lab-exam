27f43974ecf4278a0a691e2815df0c35acc448ee
jajarija434 <jajacano71@gmail.com>
2024-07-18 12:22:28 +0800

    commit 4

--- database/migrations/2024_07_18_024700_create_comment_replies_table.php
+++ database/migrations/2024_07_18_024700_create_comment_replies_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('comment_replies', function (Blueprint $table) {
             $table->id();
+            $table->string("fname");
+            $table->string("lname");
+            $table->string("email");
+            $table->string("phone")->nullable();
+            $table->string("address")->nullable();
+            $table->string("city")->nullable();
+            $table->string("province")->nullable();
+            $table->string("zip")->nullable();
+            $table->date("birthdate")->nullable();
             $table->timestamps();
         });
     }
